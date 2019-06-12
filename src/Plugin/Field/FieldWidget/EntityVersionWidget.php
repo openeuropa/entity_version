3839aa160098588513398716560f14d6b8d7472a
Hernani Borges de Freitas <hernani@Hernanis-Air.lan>
2019-06-12 14:12:34 +0100

    OPENEUROPA-1908: Do not support negative versions.

--- src/Plugin/Field/FieldWidget/EntityVersionWidget.php
+++ src/Plugin/Field/FieldWidget/EntityVersionWidget.php
@@ -28,7 +28,7 @@ class EntityVersionWidget extends WidgetBase implements WidgetInterface {
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $element['version'] = [
       '#type' => 'details',
-      '#title' => t('Version'),
+      '#title' => $this->t('Version'),
     ] + $element;
 
     $element['version']['major'] = [
