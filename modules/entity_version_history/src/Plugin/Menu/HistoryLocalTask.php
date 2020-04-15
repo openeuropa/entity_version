487e62cf2ea1ed3b58d41e8093068d020ea3f3f6
nagyad <joevagyok@gmail.com>
2020-04-15 09:36:28 +0200

    OPENEUROPA-2961: Add controller and routing for history overview.

---
+++ modules/entity_version_history/src/Plugin/Menu/HistoryLocalTask.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\entity_version_history\Plugin\Menu;
+
+use Drupal\Core\Menu\LocalTaskDefault;
+
+/**
+ * Represents a local task for history tab on a content entity.
+ */
+class HistoryLocalTask extends LocalTaskDefault {}
