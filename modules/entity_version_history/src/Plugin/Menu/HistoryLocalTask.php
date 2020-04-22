3d1a99eac2f84079eb8afbce47776e1d0c27b39f
nagyad <joevagyok@gmail.com>
2020-04-22 16:20:01 +0200

    OPENEUROPA-2961: Improvements.

--- modules/entity_version_history/src/Plugin/Menu/HistoryLocalTask.php
+++ modules/entity_version_history/src/Plugin/Menu/HistoryLocalTask.php
@@ -7,6 +7,6 @@ namespace Drupal\entity_version_history\Plugin\Menu;
 use Drupal\Core\Menu\LocalTaskDefault;
 
 /**
- * Represents a local task for history tab on a content entity.
+ * Local task link for the version history page of a content entity.
  */
 class HistoryLocalTask extends LocalTaskDefault {}
