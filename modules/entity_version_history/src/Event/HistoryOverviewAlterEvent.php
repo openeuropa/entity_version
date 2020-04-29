7bba9b1412ad90cef609d61982d6af26c7284b6c
nagyad <joevagyok@gmail.com>
2020-04-29 11:16:18 +0200

    OPENEUROPA-2962: Provide and event to alter history overview.

---
+++ modules/entity_version_history/src/Event/HistoryOverviewAlterEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\entity_version_history\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Dispatched by EntityVersionHistoryController::historyOverview().
+ *
+ * Allows to alter the render array of the history table after it has been
+ * populated with data.
+ */
+class HistoryOverviewAlterEvent extends Event {
+
+  const EVENT = 'entity_version_history.history_overview_alter_event';
+
+  /**
+   * The render array containing the history table.
+   *
+   * @var array
+   */
+  protected $historyTable;
+
+  /**
+   * Sets the history table array.
+   *
+   * @param array $table
+   *   The history table render array.
+   */
+  public function setHistoryTable(array $table): void {
+    $this->historyTable = $table;
+  }
+
+  /**
+   * Get the history table render array.
+   *
+   * @return array
+   *   Return the history table render array.
+   */
+  public function getHistoryTable(): array {
+    return $this->historyTable;
+  }
+
+}
