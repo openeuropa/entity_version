8e2620e6b78f118164c271e0125a01a47b5387af
nagyad <joevagyok@gmail.com>
2020-09-01 10:14:23 +0200

    EWPP-200: Fix update function name and add documentation.

--- entity_version.post_update.php
+++ entity_version.post_update.php
@@ -10,9 +10,13 @@ declare(strict_types = 1);
 /**
  * Apply entity version settings for bundles with version field.
  */
-function entity_version_post_update_8101() {
-  // Get entity types and bundles where the entity_version field is present.
+function entity_version_post_update_configure_settings() {
+  // Introduced new configuration to select a version field of an entity bundle
+  // as the main version field to use by the (sub)module functionalities,
+  // instead of searching for all version fields on the bundle and using the
+  // first one.
   $versioned_entity_types = \Drupal::service('entity_field.manager')->getFieldMapByFieldType('entity_version');
+  // Get entity types and bundles where the entity_version field is present.
   $version_settings_storage = \Drupal::entityTypeManager()->getStorage('entity_version_settings');
 
   // Loop through the mapping and create config entities if they don't exist.
