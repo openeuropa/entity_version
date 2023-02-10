898d5cf39b2d88725e8c6a996c6eec379e8f02c5
Francesco Sardara <francesco@tdgwebservices.com>
2023-02-10 22:07:48 +0100

    EWPP-2990: Fix code deprecations.

--- tests/src/Kernel/EntityVersionInstallerTest.php
+++ tests/src/Kernel/EntityVersionInstallerTest.php
@@ -40,7 +40,7 @@ class EntityVersionInstallerTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     $this->installConfig([
